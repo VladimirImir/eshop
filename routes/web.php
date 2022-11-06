d7d4ccedc2f4fa8a57dfa4094651da23f45c5362
VladimirImir <mimimvvv@gmail.com>
2022-11-06 07:09:17 +0200

    step 8

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,9 @@ Route::middleware(['auth','isAdmin'])->group(function () {
     Route::get('add-category', 'Admin\CategoryController@add');
     Route::post('insert-category', 'Admin\CategoryController@insert');
     Route::get('edit-prod/{id}', [CategoryController::class, 'edit']);
-    Route::put('update-category/{id}', [CategoryController::class, 'update' ] );
+    Route::put('update-category/{id}', [CategoryController::class, 'update']);
+    Route::get('delete-category/{id}', [CategoryController::class, 'destroy']);
+
 
 
 });
