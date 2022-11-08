b8060c7de64377b6f3e742d5c095cdfaf08f438d
VladimirImir <mimimvvv@gmail.com>
2022-11-08 14:42:49 +0200

    step 16

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -12,6 +13,12 @@ class FrontendController extends Controller
     {
         // featured_product - Показанные продукты.
         $featured_product = Product::where('trending','1')->take(15)->get();
-        return view('frontend.index', compact('featured_product'));
+        $trending_category = Category::where('popular','1')->take(15)->get();
+        return view('frontend.index', compact('featured_product', 'trending_category'));
+    }
+    public function category()
+    {
+        $category = Category::where('status','0')->get();
+        return view('frontend.category', compact('category'));
     }
 }
--- resources/views/layouts/inc/frontnavbar.blade.php
+++ resources/views/layouts/inc/frontnavbar.blade.php
@@ -10,14 +10,45 @@
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
+                    <a class="nav-link" href="{{ url('category') }}">Category</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
-                </li>
+                <!-- Authentication Links -->
+                @guest
+                    @if (Route::has('login'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                    @endif
+
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            {{ Auth::user()->name }}
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a class="dropdown-item" href="#">
+                                    My profile
+                                </a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </li>
+
+                        </ul>
+                    </li>
+
+                @endguest
             </ul>
         </div>
     </div>
