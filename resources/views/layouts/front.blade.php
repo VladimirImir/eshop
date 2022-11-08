b3f5e8224c90294e14f01f722aaab53a1217aed8
VladimirImir <mimimvvv@gmail.com>
2022-11-08 12:08:12 +0200

    step 15

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -17,6 +17,9 @@
     <link href="{{ asset('frontend/css/bootstrap5.css') }}" rel="stylesheet">
     <link href="{{ asset('frontend/css/custom.css') }}" rel="stylesheet">
 
+    <link href="{{ asset('frontend/css/owl.carousel.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('frontend/css/owl.theme.default.min.css') }}" rel="stylesheet">
+
 </head>
 <body>
 @include('layouts.inc.frontnavbar')
@@ -25,6 +28,8 @@
 </div>
 
 <script src="{{ asset('frontend/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('frontend/js/jquery-3.6.0.min.js') }}"></script>
+<script src="{{ asset('frontend/js/owl.carousel.min.js') }}"></script>
 
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 @if(session('status'))
