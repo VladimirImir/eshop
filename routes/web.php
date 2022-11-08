b3ed6b3c7a71f81e44bb4516f8d51154015827fe
VladimirImir <mimimvvv@gmail.com>
2022-11-08 09:27:11 +0200

    step 13

--- routes/web.php
+++ routes/web.php
@@ -38,4 +38,8 @@ Route::middleware(['auth','isAdmin'])->group(function () {
     Route::get('add-products', [ProductController::class, 'add']);
     Route::post('insert-products', [ProductController::class, 'insert']);
 
+    Route::get('edit-product/{id}', [ProductController::class, 'edit']);
+    Route::put('update-product/{id}', [ProductController::class, 'update']);
+    Route::get('delete-product/{id}', [ProductController::class, 'destroy']);
+
 });
