225676c1cf2d3968372c0f7ef43915bfc1128bf8
VladimirImir <mimimvvv@gmail.com>
2022-11-08 10:15:41 +0200

    step 14

--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,14 @@
 use App\Http\Controllers\Admin\ProductController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Frontend\FrontendController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::get('/', [FrontendController::class, 'index']);
 
 Auth::routes();
 
